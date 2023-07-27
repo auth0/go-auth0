2ba5646b2f4742eeec4c42e6fe9fcb95364401a1
Ewan Harris <ewan.harris@okta.com>
2023-07-27 11:16:10 +0100

    Apply suggestions from code review
    Co-authored-by: Rita Zerrizuela <zeta@widcket.com>

--- authentication/passwordless/passwordless.go
+++ authentication/passwordless/passwordless.go
@@ -10,7 +10,7 @@ type SendEmailRequest struct {
 	Email string `json:"email,omitempty"`
 	// Use `link` to send a link or `code` to send a verification code. If omitted, a `link` will be sent.
 	Send string `json:"send,omitempty"`
-	// Append or override the link parameters (like scope, redirect_uri, protocol, response_type), when you send a link.
+	// Append or override the link parameters (like `scope`, `redirect_uri`, `protocol`, `response_type`), when you send a link.
 	AuthParams map[string]interface{} `json:"authParams,omitempty"`
 }
 
@@ -27,7 +27,7 @@ type SendEmailResponse struct {
 // LoginWithEmailRequest defines the request body for exchanging a code requested by `SendEmail` for a token.
 type LoginWithEmailRequest struct {
 	oauth.ClientAuthentication
-	// API Identifier of the API for which you want to get an Access Token.
+	// API Identifier of the API for which you want to get an access token.
 	Audience string `json:"audience,omitempty"`
 	// The user's verification code.
 	Code string `json:"otp,omitempty"`
@@ -35,7 +35,7 @@ type LoginWithEmailRequest struct {
 	Email     string `json:"username,omitempty"`
 	GrantType string `json:"grant_type,omitempty"`
 	Realm     string `json:"realm,omitempty"`
-	// Use openid to get an ID Token, or openid profile email to also include user profile information in the ID Token.
+	// Use `openid` to get an ID token, or `openid profile email` to also include user profile information in the ID token.
 	Scope string `json:"scope,omitempty"`
 }
 
@@ -45,7 +45,7 @@ type SendSMSRequest struct {
 	Connection string `json:"connection,omitempty"`
 	// The user's phone number.
 	PhoneNumber string `json:"phone_number,omitempty"`
-	// Append or override the link parameters (like scope, redirect_uri, protocol, response_type), when you send a link.
+	// Append or override the link parameters (like `scope`, `redirect_uri`, `protocol`, `response_type`), when you send a link.
 	AuthParams map[string]interface{} `json:"authParams,omitempty"`
 }
 
@@ -62,7 +62,7 @@ type SendSMSResponse struct {
 // LoginWithSMSRequest defines the request body for exchanging a code requested by `SendSMS` for a token.
 type LoginWithSMSRequest struct {
 	oauth.ClientAuthentication
-	// API Identifier of the API for which you want to get an Access Token.
+	// API Identifier of the API for which you want to get an access token.
 	Audience string `json:"audience,omitempty"`
 	// The user's verification code.
 	Code string `json:"otp,omitempty"`
@@ -70,6 +70,6 @@ type LoginWithSMSRequest struct {
 	PhoneNumber string `json:"username,omitempty"`
 	GrantType   string `json:"grant_type,omitempty"`
 	Realm       string `json:"realm,omitempty"`
-	// Use openid to get an ID Token, or openid profile email to also include user profile information in the ID Token.
+	// Use `openid` to get an ID token, or `openid profile email` to also include user profile information in the ID token.
 	Scope string `json:"scope,omitempty"`
 }
