36edf91c36adf8e75fb2b1521e5379d0a4a48d54
Kunal Dawar <kunal.dawar@okta.com>
2024-04-22 16:17:23 +0530

    updated testcases with real world requests.

--- management/management_request_test.go
+++ management/management_request_test.go
@@ -4,88 +4,106 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	"github.com/auth0/go-auth0"
 )
 
 func TestNewRequest(t *testing.T) {
 	testCases := []struct {
 		name          string
 		method        string
-		uri           string
+		endpoint      string
 		payload       interface{}
 		options       []RequestOption
 		expectedError error
+		expectedBody  string
 	}{
 		{
-			name:          "Valid GET request",
-			method:        "GET",
-			uri:           "https://example.com",
+			name:          "Create client request",
+			method:        http.MethodPost,
+			endpoint:      api.URI("clients"),
+			payload:       &Client{Name: auth0.String("TestClient"), Description: auth0.String("Test description")},
+			options:       nil,
+			expectedError: nil,
+			expectedBody:  `{"name":"TestClient","description":"Test description"}` + "\n",
+		},
+		{
+			name:          "Read client request",
+			method:        http.MethodGet,
+			endpoint:      api.URI("clients", "c4vFzE4qeMgIEzRryyCmHcxGBZqswlbX"),
 			payload:       nil,
 			options:       nil,
 			expectedError: nil,
+			expectedBody:  "",
 		},
 		{
-			name:          "Valid POST request with payload",
-			method:        "POST",
-			uri:           "https://example.com",
-			payload:       map[string]string{"key": "value"},
-			options:       nil,
-			expectedError: nil,
-		},
-		{
-			name:          "Invalid payload encoding",
-			method:        "POST",
-			uri:           "https://example.com",
-			payload:       make(chan int), // Unsupported type
-			options:       nil,
-			expectedError: fmt.Errorf("encoding request payload failed"),
-		},
-		{
-			name:          "Valid DELETE request",
-			method:        "DELETE",
-			uri:           "https://example.com",
+			name:          "List clients request",
+			method:        http.MethodGet,
+			endpoint:      api.URI("clients"),
 			payload:       nil,
 			options:       nil,
 			expectedError: nil,
+			expectedBody:  "",
 		},
 		{
-			name:          "Valid PUT request with payload",
-			method:        "PUT",
-			uri:           "https://example.com",
-			payload:       map[string]string{"key": "value"},
+			name:          "Update client request",
+			method:        http.MethodPatch,
+			endpoint:      api.URI("clients", "c4vFzE4qeMgIEzRryyCmHcxGBZqswlbX"),
+			payload:       &Client{Name: auth0.String("UpdatedTestClient")},
 			options:       nil,
 			expectedError: nil,
+			expectedBody:  `{"name":"UpdatedTestClient"}` + "\n",
 		},
 		{
-			name:          "Valid PATCH request with payload",
-			method:        "PATCH",
-			uri:           "https://example.com",
-			payload:       map[string]string{"key": "value"},
+			name:          "Delete client request",
+			method:        http.MethodDelete,
+			endpoint:      api.URI("clients", "c4vFzE4qeMgIEzRryyCmHcxGBZqswlbX"),
+			payload:       nil,
 			options:       nil,
 			expectedError: nil,
+			expectedBody:  "",
+		},
+		{
+			name:          "Invalid payload request",
+			method:        http.MethodPost,
+			endpoint:      api.URI("clients"),
+			payload:       make(chan int),
+			options:       nil,
+			expectedError: fmt.Errorf("encoding request payload failed: json: unsupported type: chan int"),
+			expectedBody:  "",
+		},
+		{
+			name:          "Map payload request",
+			method:        http.MethodPost,
+			endpoint:      api.URI("clients"),
+			payload:       map[string]interface{}{},
+			options:       nil,
+			expectedError: nil,
+			expectedBody:  "{}\n",
 		},
 	}
 
 	for _, testCase := range testCases {
 		t.Run(testCase.name, func(t *testing.T) {
-			m := &Management{}
-			request, err := m.NewRequest(context.Background(), testCase.method, testCase.uri, testCase.payload, testCase.options...)
+			request, err := api.NewRequest(context.Background(), testCase.method, testCase.endpoint, testCase.payload, testCase.options...)
+
 			if testCase.expectedError != nil {
 				assert.Error(t, err)
-				assert.Contains(t, err.Error(), testCase.expectedError.Error())
+				assert.Equal(t, testCase.expectedError.Error(), err.Error())
 				assert.Nil(t, request)
-			} else {
-				assert.NoError(t, err)
-				assert.NotNil(t, request)
-				assert.Equal(t, "application/json", request.Header.Get("Content-Type"))
-
-				if testCase.method == "GET" || testCase.method == "DELETE" {
-					requestBody, _ := io.ReadAll(request.Body)
-					assert.Empty(t, string(requestBody))
-				}
+				return
 			}
+
+			assert.NoError(t, err)
+			assert.NotNil(t, request)
+			assert.Equal(t, "application/json", request.Header.Get("Content-Type"))
+
+			requestBody, _ := io.ReadAll(request.Body)
+			assert.Equal(t, testCase.expectedBody, (string(requestBody)))
 		})
 	}
 }
