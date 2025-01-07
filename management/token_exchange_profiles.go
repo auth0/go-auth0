8a4ff648c995ab0b18fdc04c20bffa9c89a35387
Kunal Dawar <kunal.dawar@okta.com>
2025-01-07 10:48:48 +0530

    Add Clean Patch Request

--- management/token_exchange_profiles.go
+++ management/token_exchange_profiles.go
@@ -46,6 +46,13 @@ func (t *TokenExchangeProfile) MarshalJSON() ([]byte, error) {
 	})
 }
 
+func (t *TokenExchangeProfile) cleanForPatch() *TokenExchangeProfile {
+	return &TokenExchangeProfile{
+		Name:             t.Name,
+		SubjectTokenType: t.SubjectTokenType,
+	}
+}
+
 // TokenExchangeProfileManager manages Auth0 Token Exchange Profile resources.
 type TokenExchangeProfileManager manager
 
@@ -77,6 +84,7 @@ func (m *TokenExchangeProfileManager) Read(ctx context.Context, id string, opts 
 //
 // See: https://auth0.com/docs/api/management/v2#!/token-exchange-profiles/patch_token_exchange_profile
 func (m *TokenExchangeProfileManager) Update(ctx context.Context, id string, t *TokenExchangeProfile, opts ...RequestOption) (err error) {
+	t.cleanForPatch()
 	err = m.management.Request(ctx, "PATCH", m.management.URI("token-exchange-profiles", id), t, opts...)
 	return
 }
